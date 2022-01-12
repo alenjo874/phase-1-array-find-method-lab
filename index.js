98823594dd31e6ec752cffc3652c2574ebea7689
Alen Jo <alenjo874@gmail.com>
2022-01-12 10:02:44 -0500

    commit tree bb5a564acd3000bbe8c5fe4f96322f7d192cca9d

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code your solution here
+const record = [
+  { year: "2018", result: "N/A" },
+  { year: "2017", result: "N/A" },
+  { year: "2016", result: "N/A" },
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  { year: "2012", result: "N/A" },
+  { year: "2011", result: "N/A" },
+];
+
+function superbowlWin(record) {
+  // return record.find(isWin)
+
+  // console.log(record.find(isWin));
+
+  let res = record.find(isWin);
+
+  if (res === undefined) {
+    return undefined;
+  } else {
+    return res.year;
+  }
+}
+
+function isWin(game) {
+  return game.result === "W";
+}
+
+superbowlWin(record);
