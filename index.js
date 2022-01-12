74b463a1009fe683aaeb88985781a8a287d54b39
Alen Jo <alenjo874@gmail.com>
2022-01-12 10:03:15 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function superbowlWin(record) {
+  let res = record.find(isWin);
+
+  if (res === undefined) {
+    return undefined;
+  } else {
+    return res.year;
+  }
+}
+
+function isWin(game) {
+  return game.result === "W";
+}
+
+superbowlWin(record);
